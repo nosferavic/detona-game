6ef510515700e78685dec4c50f1c31788d7120b0
victor viana <victor.deviana@outlook.com>
2024-10-30 13:38:42 -0300

    finish game

--- src/script/engine.js
+++ src/script/engine.js
@@ -4,14 +4,74 @@ const state = {
     enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
+    max: document.querySelector("#max-score"),
+    lives: document.querySelector(".live-score"),
   },
   values: {
-    timerId: null,
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
+    currentTime: 60,
+    lives: 3,
+  },
+  actions: {
+    timerId: setInterval(randomSquare, 1000),
+    countDownTimerId: setInterval(countDown, 1000),
   },
 };
+
+function loadMaxScore() {
+  const savedMaxScore = localStorage.getItem("maxScore");
+  if (savedMaxScore) {
+    state.view.max.textContent = savedMaxScore;
+  }
+}
+
+function saveMaxScore() {
+  localStorage.setItem("maxScore", state.view.max.textContent);
+}
+
+function resetGame() {
+  (state.values.result = 0),
+    (state.values.currentTime = 60),
+    (state.values.lives = 3);
+
+  state.view.score.textContent = state.values.result;
+  state.view.timeLeft.textContent = state.values.currentTime;
+  state.view.lives.textContent = `x${state.values.lives}`;
+
+  clearInterval(state.actions.timerId);
+  clearInterval(state.actions.countDownTimerId);
+
+  state.actions.timerId = setInterval(randomSquare, 1000);
+  state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
+
+function countDown() {
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+
+  if (state.values.currentTime <= 0) {
+    clearInterval(state.actions.countDownTimerId),
+      clearInterval(state.actions.timerId),
+      alert("Game Over! O seu resultado foi: " + state.values.result);
+    if (state.values.result > parseInt(state.view.max.textContent)) {
+      state.view.max.textContent = state.values.result;
+    }
+    if (state.values.result > parseInt(state.view.max.textContent)) {
+      state.view.max.textContent = state.values.result;
+      saveMaxScore();
+    }
+
+    resetGame();
+  }
+}
+
+function playSound(audioName) {
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.2;
+  audio.play();
+}
 
 function randomSquare() {
   state.view.squares.forEach((square) => {
@@ -24,10 +84,6 @@ function randomSquare() {
   state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy() {
-  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
-}
-
 function addListenerHitBox() {
   state.view.squares.forEach((square) => {
     square.addEventListener("mousedown", () => {
@@ -35,16 +91,28 @@ function addListenerHitBox() {
         state.values.result++;
         state.view.score.textContent = state.values.result;
         state.values.hitPosition = null;
-      } else if (square.id != state.values.hitPosition) {
-        state.values.result--;
-        state.view.score.textContent = state.values.result;
+        playSound("hit");
+      } else {
+        state.values.lives--;
+        state.view.lives.textContent = `x${state.values.lives}`;
+        if (state.values.lives <= 0) {
+          clearInterval(state.actions.countDownTimerId);
+          clearInterval(state.actions.timerId);
+          alert("Game Over! Você perdeu todas as suas vidas.");
+          if (state.values.result > parseInt(state.view.max.textContent)) {
+            state.view.max.textContent = state.values.result;
+            saveMaxScore();
+          }
+          resetGame();
+        }
       }
     });
   });
 }
 function initialize() {
-  moveEnemy();
+  loadMaxScore();
   addListenerHitBox();
+  resetGame();
 }
 
 initialize();
