a479010da7667a33a1d77a9e804769157d29b241
victor viana <victor.deviana@outlook.com>
2024-10-30 00:52:35 -0300

    add score engine

--- src/script/engine.js
+++ src/script/engine.js
@@ -5,7 +5,12 @@ const state = {
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
   },
-  values: {},
+  values: {
+    timerId: null,
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+  },
 };
 
 function randomSquare() {
@@ -16,11 +21,30 @@ function randomSquare() {
   let randomNumber = Math.floor(Math.random() * 9);
   let randomSquare = state.view.squares[randomNumber];
   randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
 }
 
+function moveEnemy() {
+  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
 
+function addListenerHitBox() {
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+      } else if (square.id != state.values.hitPosition) {
+        state.values.result--;
+        state.view.score.textContent = state.values.result;
+      }
+    });
+  });
+}
 function initialize() {
-  randomSquare() 
+  moveEnemy();
+  addListenerHitBox();
 }
 
 initialize();
