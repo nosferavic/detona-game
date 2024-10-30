7db266681e9e1ee0fc913303d86cbd2ae12be2c1
victor viana <victor.deviana@outlook.com>
2024-10-30 00:32:28 -0300

    start engine

---
+++ src/script/engine.js
@@ -0,0 +1,26 @@
+const state = {
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+  },
+  values: {},
+};
+
+function randomSquare() {
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+  });
+
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+}
+
+
+function initialize() {
+  randomSquare() 
+}
+
+initialize();
